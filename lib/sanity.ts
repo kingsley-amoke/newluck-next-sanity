4ddfd84693f5ebf2f1ad3724598a3457b1709922
kingsley-amoke <klordbravo@gmail.com>
2024-07-09 21:10:27 +0100

    minor updates

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,10 +1,14 @@
 import ImageUrlBuilder from "@sanity/image-url";
 import { createClient } from "next-sanity";
 
+const projectId = process.env.EXPO_PUBLIC_SANITY_PROJECT_ID as string
+const dataset = process.env.EXPO_PUBLIC_SANITY_DATASET as string
+const apiVersion = process.env.EXPO_PUBLIC_SANITY_API_VERSON as string
+
 export const client = createClient({
-  projectId: "ah285ksp",
-  dataset: "production",
-  apiVersion: "2022-03-07",
+  projectId: projectId,
+  dataset: dataset,
+  apiVersion: apiVersion,
   useCdn: true,
 });
 
